dbeedbc592e17d0fa38eb0e205a5005dda1cb87e
Dharunya <88583525+dharunya0609@users.noreply.github.com>
2022-08-25 21:37:35 +0530

    Create Frequency.c

---
+++ Frequency.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main()
+{
+	int a[5],f[5];
+	int i,j,n,cnt=0;
+	printf("Enter array size:\n");
+	scanf("%d",&n);
+	
+	printf("Enter array elements:\n");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+		f[i]=-1;
+	}
+
+	for(i=0;i<n;i++)
+	{
+	   cnt=1;
+	   for(j=i+1;j<n;j++)
+	   {
+	   	 if(a[i]==a[j])
+	   	 {
+	   	   	cnt++;
+	   	   	f[j]=0;
+		 }
+	   }
+	   if(f[i]!=0)
+	   {
+	   	 f[i]=cnt;
+	   }
+	   
+	}
+	
+	
+	for(i=0;i<n;i++)
+	   {
+	   	 if(f[i]!=0)
+	   	 {
+	   	    printf("Element %d occured %d times ",a[i],f[i]);	
+		 }
+	   	
+	   }  
+	
+	
+	return 0;
+}
